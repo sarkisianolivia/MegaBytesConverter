bf9c594147e68f97655c0df865455d269128713d
Olivia Sarkisian <115608329+sarkisianolivia@users.noreply.github.com>
2022-10-31 22:48:21 -0500

    Add files via upload

---
+++ MegaBytesConverter.java
@@ -0,0 +1,16 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int megaBytes = kiloBytes / 1024;
+
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
+
+            int remainingKiloBytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes + " KB");
+        }
+
+    }
+}
+
